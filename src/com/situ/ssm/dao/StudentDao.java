f77da44d3bf7d5d4f743c65daa074170a2885373
linjia12345 <17854268530@163.com>
2018-02-01 12:12:53 +0800

    第几次提交

---
+++ src/com/situ/ssm/dao/StudentDao.java
@@ -0,0 +1,10 @@
+package com.situ.ssm.dao;  
+  
+import java.util.List;  
+  
+import com.situ.ssm.entity.Student;  
+  
+public interface StudentDao {  
+	List<Student> findAll();  
+	
+}  
---
+++ src/com/situ/ssm/service/IStudentService.java
@@ -0,0 +1,11 @@
+package com.situ.ssm.service;  
+   
+ import java.util.List;  
+   
+ import com.situ.ssm.entity.Student;  
+   
+ public interface IStudentService {  
+   
+ 	List<Student> findAll(); 
+   
+  }  
